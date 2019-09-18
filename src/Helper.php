0584a751ec1c3776c9cf2331e0d7c796800b0cd0
zqbx <597050481@qq.com>
2019-09-18 23:00:44 +0800

    'helper'

---
+++ src/Helper.php
@@ -0,0 +1,11 @@
+<?php
+	
+namespace zqbx;
+
+class Helper {
+	
+	static public function test() {
+		return "111111111";
+	}
+
+}
---
+++ test/test.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once '../vendor/autoload.php';
+
+use zqbx\Helper;
+
+var_dump(Helper::test());
